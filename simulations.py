16819f7fcc30d2bc1c05bac16fa16e0140a73108
lluismer <lluismergo@outlook.com>
2026-01-21 12:57:08 -0600

    Decided to make a new file for all the dataclasses

--- simulations.py
+++ simulations.py
@@ -1,53 +1,6 @@
 #Import all functions from dice roll
 from dice_roll import *
 import matplotlib.pyplot as plt
-from dataclasses import dataclass, field
-from typing import List, Set
-from enum import IntEnum
-
-class DamageType(IntEnum):
-    BLUDGEONING = 0
-    PIERCING = 1
-    SLASHING = 2
-    FIRE = 3
-    COLD = 4
-    LIGHTNING = 5
-    ACID = 6
-    POISON = 7
-    PSYCHIC = 8
-    NECROTIC = 9
-    RADIANT = 10
-    THUNDER = 11
-    FORCE = 12
-
-@dataclass
-class Dice:
-    number: int
-    faces: int
-
-@dataclass
-class Damage:
-    dice: Dice
-    dmg_modifier: int
-    dmg_type: DamageType
-
-@dataclass
-class ToHit:
-    hit_mod: int
-    bonus_dice: List[Dice]
-
-@dataclass
-class Attack:
-    to_hit: ToHit
-    damage: List[Damage]
-
-@dataclass
-class Defender:
-    ac: int
-    resistance: Set[DamageType]
-    immunity: Set[DamageType]
-
-
 
 
 def roll_list(faceNum,testNum = 100000,diceMod=0,toBeat=0):
